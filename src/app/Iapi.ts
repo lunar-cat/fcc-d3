a05c6756d08f1a7f2de688fe24b0eae61cf4409e
Jose Pradenas <josepradenas@outlook.es>
2021-09-03 15:07:07 -0400

    added ts interface's for json data

---
+++ src/app/Iapi.ts
@@ -0,0 +1,71 @@
+interface BarChart {
+  data: [string, number][]
+}
+interface ScatterplotGraph {
+  Time: string,
+  Place: number,
+  Seconds: number,
+  Name: string,
+  Year: number,
+  Nationality: string,
+  Doping: string,
+  URL: string,
+}
+interface HeatMap {
+  baseTemperature: number,
+  monthlyVariance: { year: number, month: number, variance: number }[]
+}
+interface ChoroplethMap {
+  fips: number,
+  state: string,
+  area_name: string,
+  bachelorsOrHiger: number,
+}
+interface ChoroplethTopology {
+  objects: {
+    counties: {
+      type: string,
+      geometries: {
+        type: string,
+        id: number,
+        arcs: number[][]
+      }[]
+    },
+    states: {
+      type: string,
+      geometries: {
+        type: string,
+        arcs: number[][][],
+        id: string
+      }[]
+    },
+    nation: {
+      type: string,
+      geometries: {
+        type: string,
+        arcs: number[][][]
+      }[]
+    },
+  },
+  arcs: number[][][],
+  bbox: number[],
+  transform: {
+    scale: number[],
+    translate: number[]
+  }
+}
+interface TreemapDiagram {
+  name: string,
+  children: {
+    name: string,
+    children: {
+      name: string,
+      category: string,
+      value: string
+    }[]
+  }[]
+}
+export {
+  BarChart, ScatterplotGraph, HeatMap,
+  ChoroplethMap, ChoroplethTopology, TreemapDiagram
+};
