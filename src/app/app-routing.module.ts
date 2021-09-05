bb23a67e1630b2d3344a5f1e334f10e7d91739f4
Jose Pradenas <josepradenas@outlook.es>
2021-09-05 12:02:43 -0300

    testing routing module & routerLinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { ChoroplethMapComponent } from './choropleth-map/choropleth-map.component';
+import { HeatMapComponent } from './heat-map/heat-map.component';
+import { ScatterplotGraphComponent } from './scatterplot-graph/scatterplot-graph.component';
+import { TreemapDiagramComponent } from './treemap-diagram/treemap-diagram.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bar-chart', component: BarChartComponent},
+  {path: 'choropleth-map', component: ChoroplethMapComponent},
+  {path: 'heat-map', component: HeatMapComponent},
+  {path: 'scatterplot-graph', component: ScatterplotGraphComponent},
+  {path: 'treemap-diagram', component: TreemapDiagramComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
