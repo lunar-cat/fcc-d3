92b8093707463c134ac030c235011186eed9c013
Jose Pradenas <josepradenas@outlook.es>
2021-09-06 15:39:24 -0300

    added explanation comments

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -36,6 +36,10 @@ export class BarChartComponent implements OnInit {
     if (!xMin || !xMax || !yMin || !yMax) return;
     const xScale = d3.scaleTime().domain([xMin, xMax]).range([p, w - p]);
     const yScale = d3.scaleLinear().domain([0, yMax]).range([h - p, p]);
+    // yScale 0 === 550 (h - p), por lo cual es valor más cercano a 0 es 550
+    // Dejándole en la Y 550, desde arriba hacia abajo, por tanto más cercano
+    // al 0 del axis
+    // de ahí, 
     const svg = d3.select('#svgContainer')
       .append('svg')
       .attr('width', w)
@@ -73,7 +77,7 @@ export class BarChartComponent implements OnInit {
         div.style.visibility = 'visible';
         div.style.top = `${yUser}px`;
         div.style.left = `${xUser + 20}px`;
-        div.textContent = `Fecha: ${date}\n Valor: $${value}`;
+        div.textContent = `Fecha: ${date}\n Valor: $${parseInt(value).toFixed()}`;
         div.setAttribute('data-date', date);
       })
       .on('mouseout', e => {
@@ -88,7 +92,10 @@ export class BarChartComponent implements OnInit {
       .attr('data-date', d => d[0])
       .attr('data-gdp', d => d[1])
       .attr('fill', 'purple')
-      .attr('height', d => yScale(0) - yScale(d[1]))
+      .attr('height', d => {
+        console.log(yScale(d[1]));
+        return yScale(0) - yScale(d[1]);
+      }) // yScale(0) es === que h - p === 550, ojo h-p no yScale(h - p)
       .attr('width', barWidth / 1.5)
       .attr('y', d => yScale(d[1]))
       .attr('x', d => xScale(new Date(d[0])));
