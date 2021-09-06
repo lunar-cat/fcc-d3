028319f34887236ff2cb05dc1171d54b8c76693a
Jose Pradenas <josepradenas@outlook.es>
2021-09-05 21:58:46 -0300

    first try create-svg in bar-chart  comp

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 import { ChartService } from '../chart.service';
 import { BarChart } from '../Iapi';
 
@@ -9,6 +10,7 @@ import { BarChart } from '../Iapi';
 })
 export class BarChartComponent implements OnInit {
   data!: BarChart;
+  svg!: SVGSVGElement;
   constructor(private chartService: ChartService) { }
 
   ngOnInit(): void {
@@ -19,6 +21,27 @@ export class BarChartComponent implements OnInit {
       .subscribe(response => {
         console.log(response);
         this.data = response[0] as BarChart;
+        this.createSVG();
       });
+  }
+  createSVG(): void {
+    if (!this.data) return;
+    const [w, h] = [800, 400];
+    const barW = w / this.data.data.length;
+    const xValues = this.data.data.map(x => new Date(x[0]));
+    const yValues = this.data.data.map(y => y[1]);
+    const [xMin, xMax] = [d3.min(xValues), d3.max(xValues)];
+    const [yMin, yMax] = [d3.min(yValues), d3.max(yValues)];
+
+    if (!xMin || !xMax || !yMin || !yMax) return;
+    const xScale = d3.scaleTime().domain([xMin, xMax]).range([0, w]);
+    const yScale = d3.scaleLinear().domain([0, yMax]).range([h, 0]);
+    const svg = d3.select('#svgContainer')
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h)
+      .style('border', '2px solid black')
+      .node();
+    if (svg !== null) this.svg = svg;
   }
 }
