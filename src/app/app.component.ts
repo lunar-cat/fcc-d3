c7601f9a47631ea18a2201809f850b467484a13a
Jose Pradenas <josepradenas@outlook.es>
2021-09-12 15:42:24 -0300

    added paperCSS & title service & appmethod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fcc-charts';
+  constructor(private titleService: Title) { }
+  
+  setTitle(title: string): void {
+    this.titleService.setTitle(title);
+  }
 }
